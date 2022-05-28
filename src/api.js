6e16104f3bb09bb8d74c59e0e1c7e1d5d0b387b9
miss-f-g <fatemeghafari98@gmail.com>
2022-05-28 15:34:45 +0430

    1.wirte api setting

---
+++ src/api.js
@@ -0,0 +1,35 @@
+const base_url = `https://api.rawg.io/api/`;
+
+// getting date
+const getCurrentMonth = () => {
+  const month = new Date().getMonth() + 1;
+  //   console.log(month);
+  if (month < 10) {
+    return `0${month}`;
+  } else {
+    return month;
+  }
+};
+
+const getCurrentDay = () => {
+  const day = new Date().getDay();
+  //   console.log(day);
+  if (day < 10) {
+    return `0${day}`;
+  } else {
+    return day;
+  }
+};
+
+const currentYear = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+
+const lastYear = `${currentYear - 1} -${currentMonth}-${currentDay}`;
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1} -${currentMonth}-${currentDay}`;
+
+const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+
+
+export const popularGamesURL=()=> `${base_url}${popular_games}?key=${process.env.REACT_APP_API_KEY}`
