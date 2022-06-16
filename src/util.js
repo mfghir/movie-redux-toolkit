f1c8b5f231020b9e1b03590e880c2c2d622e79b5
Fateme Ghafari <83067447+mfghir@users.noreply.github.com>
2022-06-16 12:20:53 +0430

    Delete util.js

--- src/util.js
+++
@@ -1,9 +0,0 @@
-export const smallImage = (imagePath, size) => {
-    const image = imagePath.match(/media\/screenshots/)
-      ? imagePath.replace(
-          "media/screenshots",
-          `media/resize/${size}/-/screenshots`
-        )
-      : imagePath.replace("/media/games/", `/media/resize/${size}/-/games/`);
-    return image;
-  };
